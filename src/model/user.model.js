3d9040bbf235ae365f46d55ec5613640005f419e
swarnikaraj <swarnikarajsingh@gmail.com>
2021-12-13 16:07:44 +0530

    gbki

---
+++ src/model/user.model.js
@@ -0,0 +1,21 @@
+
+
+const {Schema, model}= require('mongoose')
+
+
+const userSchema= new Schema({
+
+    name:{type:String, required:true},
+    email:{type:String,required:true, unique:true},
+    profile_photo_url:{type:String,required:true},
+    password:{type:String,required:true},
+
+    roles:{type:String,required:true}
+
+},{
+    versionKey:false,
+    timestamps:true
+})
+
+module.exports=model('user',userSchema)
+
