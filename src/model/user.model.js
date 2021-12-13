bbc923ff99ba8a057313e1cb31d6d7bcd5db3186
swarnikaraj <swarnikarajsingh@gmail.com>
2021-12-13 16:21:54 +0530

    kjnk
    lhnk

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,6 +1,8 @@
 
 
 const {Schema, model}= require('mongoose')
+const bcrypt=require('bcryptjs')
+const jwt=require("jsonwebtoken")
 
 
 const userSchema= new Schema({
@@ -16,6 +18,52 @@ const userSchema= new Schema({
     versionKey:false,
     timestamps:true
 })
+
+
+
+
+
+
+userSchema.pre("save",function (next){
+    if(!this.isModified("password")) return next();
+    
+    bcrypt.hash(this.password,10,(err,hash)=>{
+
+        this.password=hash;
+        return next()
+
+    });
+    
+    
+
+
+});
+
+
+userSchema.methods.checkPassword=function (password){
+   
+
+    return new Promise((resolve,reject)=>{
+
+
+        bcrypt.compare(password,this.password,function(err,res){
+
+            if(err) return reject(err);
+
+
+            return resolve(res)
+
+        });
+
+
+    })
+
+
+
+   
+
+
+}
 
 module.exports=model('user',userSchema)
 
